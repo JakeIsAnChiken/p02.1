594ff72919f349785eb533527916cc5ac384a57a
Jake.Mcgough <jake.mcgough18@venerablebede.co.uk>
2016-09-30 18:06:37 +0100

    i have done the changes

--- biggest.py
+++ biggest.py
@@ -26,10 +26,10 @@ def run_tests():
 # Edit this code
 def biggest(a, b, c):
 
-    if a > b and a > c:
+    if a = b and a = c:
         print(a)
 
-    elif b > a and b > c:
+    elif b = a and b = c:
         print(b)
 
     else:
